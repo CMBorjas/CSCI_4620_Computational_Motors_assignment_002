6a2df8439ca76a10581ec44485f01bfa577b0f12
CMBorjas <118933346+CMBorjas@users.noreply.github.com>
2025-04-19 13:07:21 -0600

    Add PD controller files and ArmPDExample project

---
+++ ArmPDExample/ArmPDExample/main_script_incomplete.py
@@ -0,0 +1,69 @@
+import opensim as osim
+import sys
+import math
+import numpy as np
+
+model_filename = 'ue_torque.osim'
+
+
+model = osim.Model(model_filename)
+stepsize = 0.005;
+nbr_timesteps = 150;
+
+state = model.initSystem()
+muscleController = osim.PrescribedController();
+muscleSet = model.getMuscles();
+torqueSet = model.getActuators();
+nbr_muscles = muscleSet.getSize();
+
+for j in range(torqueSet.getSize()):
+	func = osim.Constant(1.0)
+	muscleController.addActuator(torqueSet.get(j))
+	muscleController.prescribeControlForActuator(torqueSet.get(j).getName(), func)
+model.addController(muscleController)
+
+#model.setUseVisualizer(True)
+
+state = model.initSystem() 
+coords = model.updCoordinateSet();
+
+model.equilibrateMuscles(state)
+manager = osim.Manager(model)
+manager.initialize(state)
+
+f = 0;
+
+total_time = stepsize * nbr_timesteps;
+
+com_i = model.calcMassCenterPosition(state);
+max_com = -math.inf;
+
+for i in range(0,nbr_timesteps+1):
+	t = state.getTime() + stepsize
+	state = manager.integrate(t)
+		
+	brain = osim.PrescribedController.safeDownCast(model.getControllerSet().get(0))
+	functionSet = brain.get_ControlFunctions()
+
+	shoulder_elv = coords.get('shoulder_elv').getValue(state);
+	shoulder_elv_v = coords.get('shoulder_elv').getSpeedValue(state);
+
+    #INCLUDE CODE FOR ELBOW. 
+    #The angle is elbow_flexion.
+
+	for j in range(functionSet.getSize()):
+		func = osim.Constant.safeDownCast(functionSet.get(j))
+		
+		if j == 1:
+			func.setValue(-20 * (shoulder_elv - 1.5708) - 1 * shoulder_elv_v)
+		#elif j == 3:
+		#	INCLUDE CODE FOR ELBOW  
+
+
+	model.realizeDynamics(state);
+	com = model.calcMassCenterPosition(state);
+
+
+statesDegrees = manager.getStateStorage();
+statesDegrees.printToFile("PDControl.sto", "w");
+
